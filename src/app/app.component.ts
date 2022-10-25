0a10d9dfc3bd4797f2bad256d797e6cc83654951
bngams <n.boris@devidia.net>
2022-10-25 08:54:08 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+// TS Decorator
+// Component config
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-app';
+  customDate!: Date;
+  date = new Date();
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.customDate = new Date();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExponentialStrengthPipe } from './pipes/exponential-strength.pipe';
 
+// TS Decorator
+// @NgModule <=> ES Module
 @NgModule({
+  // declares HTMLElement, declares HTML Syntax
   declarations: [
-    AppComponent
+    AppComponent,
+    ExponentialStrengthPipe
   ],
+
+  // imports features
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+
+  // exports features to be loaded in other modules imports
+  // exports: []
+
+  // configure DI
   providers: [],
+
+  // app launch
   bootstrap: [AppComponent]
 })
 export class AppModule { }
